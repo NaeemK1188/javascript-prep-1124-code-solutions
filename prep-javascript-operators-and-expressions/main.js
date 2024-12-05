59ce26c1899d63f6ef98a6d6853afb9df6786965
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-05 03:40:49 +0000

    completed prep-javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,47 @@
+let number1 = 7;
+let number2 = 4;
+let product = number1 * number2;
+
+console.log('value of product:', product);
+console.log('type of product', typeof product);
+
+let charge = 10;
+let payment = 4.5;
+let amountRemaining = charge - payment;
+
+console.log('value of amount Remaining:', amountRemaining);
+console.log('type of amount Remaining:', typeof amountRemaining);
+
+let tests = 100;
+let assignments = 80;
+let final = 90;
+let grade = (tests + assignments + final) / 3; // average grade
+
+console.log('value of grade:', grade);
+console.log('type of grade:', typeof grade);
+
+let firstName = 'Ron';
+let lastName = 'Burgundy';
+let fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+let pH = 3;
+//let isAcidic = (pH < 7); // its acidic because its less than 7 ph level or its true because 3 < 7
+let isAcidic = pH > 7; // its not acidic because its less than 7 or its false because 3 is not greater tan 7
+
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+let numberOfSoldiers = 300;
+let isSparta = numberOfSoldiers !== 300; // false because its equal each other numberofsoldiers and 300
+
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+let nickName = fullName;
+nickName = nickName + ' is the GOAT';
+
+console.log('value of nickName:', nickName);
+console.log('type of nickName:', typeof nickName);
---
+++ prep-javascript-operators-and-expressions/prep-javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,53 @@
+# prep-javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What data type is returned by an arithmetic operation?
+
+  number type or integer
+
+- What is string concatenation?
+
+  is adding two strings together
+
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+
+  addition in arithmetic operation or concatenation in strings
+
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+
+  boolean data type true/false values
+
+- What does the `+=` "plus-equals" operator do?
+
+  increment the value by itself like y += 1 => y = y + 1
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
